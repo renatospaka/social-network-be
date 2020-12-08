03b443c056df97bda5e6a5af6b06844c3311cad7
Renato Spakauskas <renatospaka@gmail.com>
2020-12-08 18:27:26 -0300

    Initiating authentication - JWT

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,12 +1,10 @@
 const { validationResult } = require('express-validator');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const User = require('../models/user');
 
 exports.signup = (req, res, next) => {
-  console.log(req.body.name);
-  console.log(req.body.password);
-  console.log(req.body.email);
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     const error = new Error('Validation failed.')
@@ -31,6 +29,52 @@ exports.signup = (req, res, next) => {
       res
       .status(201)
       .json({ message: 'User created successfully.', userId: newUser._id });
+    })
+    .catch(err => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      };
+      next(err);
+    });
+};
+
+exports.login = (req, res, next) => {
+  const email = req.body.email;
+  const password = req.body.password;
+  console.log('controller', req.body.password);
+  console.log('controller', req.body.email);
+  let loadedUser;
+  User.findOne({ email: email })
+    .then(user => {
+      if (!user) {
+        const error = new Error('User or password invalid.')
+        error.statusCode = 401;
+        error.data = errors.array();
+        throw error;
+      }
+      loadedUser = user;
+      return bcrypt.compare(password, user.password);
+    })
+    .then(isEqual => {
+      if (!isEqual) {
+        const error = new Error('User or password invalid.')
+        error.statusCode = 401;
+        error.data = errors.array();
+        throw error;
+      };
+
+      const token = jwt.sign(
+        {
+          email: loadedUser.email, 
+          userId: loadedUser._id.toString()
+        }, 
+        'someSuperHugeSecretFromHeaven', 
+        { expiresIn: '1h' }
+      );
+
+      res
+        .status(200)
+        .json({ token: token, userId: loadedUser._id.toString() });
     })
     .catch(err => {
       if (!err.statusCode) {
