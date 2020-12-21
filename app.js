97980c4ffacfc06f8466498bfa3d7a3dfaa7cf28
Renato Spakauskas <renatospaka@gmail.com>
2020-12-21 18:15:03 -0300

    Fixing API port from 8080 to 8091

--- app.js
+++ app.js
@@ -59,7 +59,7 @@ require('dotenv').config()
 const uri = process.env.MONGODB_URI;
 mongoose.connect(uri, { useNewUrlParser:true, useUnifiedTopology: true })
   .then(result => {
-    app.listen(8080);
+    app.listen(8091);
   })
   .catch(err => console.log(err))
 
--- controllers/feed.js
+++ controllers/feed.js
@@ -3,6 +3,7 @@ const path = require('path');
 const { validationResult } = require('express-validator');
 
 const Post = require('../models/post');
+const User = require('../models/user');
 
 exports.getPosts = (req, res, next) => {
   const currentPage = req.query.page || 1;
@@ -46,20 +47,31 @@ exports.createPost = (req, res, next) => {
   const title = req.body.title;
   const content = req.body.content;
   const imageUrl = req.file.path;
+  let creator;
   const post = new Post({
     title: title, 
     content: content,
     imageUrl: imageUrl,
-    creator: {
-      name: 'Renato, o bonzão'
-    },
+    creator: req.userId
   });
   post
     .save()
     .then(result => {
+      return User.findById(req.userId);
+    })
+    .then(user => {
+      creator = user;
+      user.posts.push(post);
+      return user.save();
+    })
+    .then(result => {
       res
         .status(201)
-        .json({ message: 'Post created successfully', post: post });
+        .json({ 
+          message: 'Post created successfully', 
+          post: post,
+          creator: { _id: creator._id, name: creator.name }
+        });
     })
     .catch(err => {
       if (!err.statusCode) {
