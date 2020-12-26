cdcb418fc7247a7a7bd43393205050c84c1911d1
Renato Spakauskas <renatospaka@gmail.com>
2020-12-26 17:55:37 -0300

    Refactoring to async/await getPost & createPost

--- controllers/feed.js
+++ controllers/feed.js
@@ -5,34 +5,27 @@ const { validationResult } = require('express-validator');
 const Post = require('../models/post');
 const User = require('../models/user');
 
-exports.getPosts = (req, res, next) => {
+exports.getPosts = async (req, res, next) => {
   const currentPage = req.query.page || 1;
   const perPage = 2;
-  let totalItems;
-  Post
-    .find()
-    .countDocuments()
-    .then(count => {
-      totalItems = count;
-      return Post
-        .find()
-        .skip((currentPage - 1) * perPage)
-        .limit(perPage);
-    })
-    .then(posts => {
-      res
-        .status(200)
-        .json({ message: 'Fetched posts successfully.', posts: posts, totalItems: totalItems });
-    })
-    .catch(err => {
-      if (!err.statusCode) {
-        err.statusCode = 500;
-      };
-      next(err);
-    });
+  
+  try {
+    const totalItems = await Post.find().countDocuments();
+    const posts = await Post.find()
+      .skip((currentPage - 1) * perPage)
+      .limit(perPage);
+
+    res.status(200)
+      .json({ message: 'Fetched posts successfully.', posts: posts, totalItems: totalItems });
+  } catch (err) {
+    if (!err.statusCode) {
+      err.statusCode = 500;
+    };
+    next(err);
+  }
 };
 
-exports.createPost = (req, res, next) => {
+exports.createPost = async (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     const error = new Error('Validation failed. Entered data is invalid.');
@@ -54,31 +47,25 @@ exports.createPost = (req, res, next) => {
     imageUrl: imageUrl,
     creator: req.userId
   });
-  post
-    .save()
-    .then(result => {
-      return User.findById(req.userId);
-    })
-    .then(user => {
-      creator = user;
-      user.posts.push(post);
-      return user.save();
-    })
-    .then(result => {
-      res
-        .status(201)
-        .json({ 
-          message: 'Post created successfully', 
-          post: post,
-          creator: { _id: creator._id, name: creator.name }
-        });
-    })
-    .catch(err => {
-      if (!err.statusCode) {
-        err.statusCode = 500;
-      };
-      next(err);
-    });
+    
+  try {
+    const creator = await post.save();
+    let user = await User.findById(req.userId);
+    user.posts.push(post);
+    user = await creator.save();
+
+    res.status(201)
+      .json({ 
+        message: 'Post created successfully', 
+        post: post,
+        creator: { _id: creator._id, name: creator.name }
+      }); 
+  } catch (err) {
+    if (!err.statusCode) {
+      err.statusCode = 500;
+    };
+    next(err);
+  }
 };
 
 exports.getPost = (req, res, next) => {
